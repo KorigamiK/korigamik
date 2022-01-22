c788764810a4c6a901cb986f942db3c30374a961
KorigamiK <korigamik@gmail.com>
2022-01-23 01:46:16 +0530

    make basic layout

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import "./styles/root.css";
 import "./styles/style.css";
 
 const app = document.getElementById("app")!;
 
-app.innerHTML = `
-  <h1>Hello Everyone!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+// app.innerHTML = `
+//   <h1>This is magic!</h1>
+// `;
