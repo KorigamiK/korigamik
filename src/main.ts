4d65279c2e823b2a22a15916c7fcc7301e957b7f
KorigamiK <korigamik@gmail.com>
2022-01-23 00:18:31 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "./styles/style.css";
+
+const app = document.getElementById("app")!;
+
+app.innerHTML = `
+  <h1>Hello Everyone!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
