eb439e3061804596968c57fdc9a7e9464962210a
KorigamiK <korigamik@gmail.com>
2022-01-24 17:31:10 +0530

    make some progress

--- src/main.ts
+++ src/main.ts
@@ -60,3 +60,83 @@ window.onscroll = function () {
     return;
   }
 };
+
+const createIcon = (e: Element) => {
+  e.setAttribute(
+    "src",
+    `https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/${e.getAttribute(
+      "icon"
+    )}.svg`
+  );
+  e.setAttribute("alt", e.getAttribute("icon")!);
+  e.setAttribute("align", "center");
+  if (e.getAttribute("link")) {
+    (e as HTMLElement).onclick = () =>
+      (window.location.href = e.getAttribute("link")!);
+  }
+};
+
+document.querySelectorAll("[icon]").forEach(createIcon);
+
+interface Iskills {
+  name: string;
+  value: number;
+  svg?: string;
+}
+
+const skills: Iskills[] = [
+  {
+    name: "HTML",
+    value: 100,
+    svg: "html5",
+  },
+  {
+    name: "Python",
+    value: 100,
+  },
+  {
+    name: "TypeScript",
+    value: 100,
+  },
+  {
+    name: "C++",
+    value: 50,
+    svg: "cplusplus",
+  },
+  {
+    name: "Go",
+    value: 40,
+  },
+  {
+    name: "React",
+    value: 80,
+  },
+  {
+    name: "ReactNative",
+    value: 80,
+    svg: "reactos",
+  },
+  {
+    name: "Flutter",
+    value: 40,
+  },
+];
+
+const progressSection = document.querySelector(".progress-section")!;
+
+for (const skill of skills) {
+  const img = document.createElement("img");
+  const p = document.createElement("p");
+  img.setAttribute("icon", skill.svg ? skill.svg : skill.name.toLowerCase());
+  img.setAttribute("width", "40");
+  createIcon(img);
+  p.innerText = skill.name;
+  const div = document.createElement("div");
+  div.appendChild(img);
+  div.appendChild(p);
+  progressSection.appendChild(div);
+  const progressbar = document.createElement("progress");
+  progressbar.setAttribute("max", "100");
+  progressbar.setAttribute("value", skill.value.toString());
+  progressSection.appendChild(progressbar);
+}
